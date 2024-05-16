2b34c70bdaefd8cec927f6c1c6dc31f3f780737d
BoredDarkness <gwinnblaidd@gmail.com>
2024-05-16 13:48:03 +0300

    Summary

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const input = document.querySelector("#name-input");
-  const output = document.querySelector("#name-output");
-
-  input.addEventListener("input", () => {
-    const trimmedInput = input.value.trim();
-    output.textContent = trimmedInput || "Anonymous";
-  });
-});
